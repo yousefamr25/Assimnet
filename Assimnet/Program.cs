f3cf92e249d84f5a3d01366fa249e40ce3897ec8
yousefamr25 <ucefamr32@gmail.com>
2024-07-29 17:56:29 +0300

    Update Program.cs

--- Assimnet/Program.cs
+++ Assimnet/Program.cs
@@ -4,9 +4,35 @@ namespace Assimnet
 {
     internal class Program
     {
-        static void Main(string[] args)
+               static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Q1 Bubble Sort
+            //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            //Console.WriteLine("Original array: " + string.Join(", ", arr));
+
+            //BubbleSort.OptimizedBubbleSort(arr);
+
+            //Console.WriteLine("Sorted array: " + string.Join(", ", arr));
+            #endregion
+
+            #region Q2 generic 
+            //var intRange = new Range<int>(5, 10);
+            //Console.WriteLine($"Is 7 in range: {intRange.IsInRange(7)}");
+            //Console.WriteLine($"Length of range: {intRange.Length()}"); 
+
+            //var doubleRange = new Range<double>(1.5, 4.5);
+            //Console.WriteLine($"Is 3.2 in range: {doubleRange.IsInRange(3.2)}");
+            //Console.WriteLine($"Length of range: {doubleRange.Length()}");
+
+            //var dateRange = new Range<DateTime>(new DateTime(2020, 1, 1), new DateTime(2020, 12, 31));
+            //TimeSpan length = dateRange.Length();
+            //Console.WriteLine($"Is 2020-06-15 in range: {dateRange.IsInRange(new DateTime(2020, 6, 15))}");
+            //Console.WriteLine($"Length of range: {length.Days} days");
+
+            #endregion
+
         }
+
     }
 }
